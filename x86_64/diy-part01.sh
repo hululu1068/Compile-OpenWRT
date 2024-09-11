9e34683cc86d87b48356bf2c5e6b7a7268d8a295
hululu1068 <xjianglin@gmail.com>
2024-09-11 17:09:35 +0800

    update x86_64/diy-part01.sh

--- x86_64/diy-part01.sh
+++ x86_64/diy-part01.sh
@@ -19,8 +19,9 @@ sed -i '1s|.*|src-git packages https://github.com/immortalwrt/packages.git;openw
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-# echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 
 # themes添加（svn co 命令意思：指定版本如https://github）
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/custom-package/luci-theme-argon
@@ -32,7 +33,7 @@ sed -i '1s|.*|src-git packages https://github.com/immortalwrt/packages.git;openw
 # git clone https://github.com/pymumu/smartdns.git package/custom-package/smartdns
 # git clone https://github.com/sirpdboy/luci-app-netdata.git package/custom-package/luci-app-netdata
 # Passwall2 | 采用clone的方式
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/custom-package/Passwall-Packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/custom-package/Passwall2
-#Open Clash
-git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git package/custom-package/OpenClash
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/custom-package/Passwall-Packages
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/custom-package/Passwall2
+# Open Clash
+# git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git package/custom-package/OpenClash
